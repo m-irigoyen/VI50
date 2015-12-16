d18259b96f82fe6eb16d4f755e7ead5c583dd436
ValentinLeMorvan <valentinlemorvan@gmail.com>
2015-12-16 18:48:32 +0100

    The land is generated automatically and moves.

---
+++ Assets/MovingLand.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingLand : MonoBehaviour {
+    
+    public Transform canyonPrefab;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        Transform firstCanyon = Instantiate(canyonPrefab);
+
+        firstCanyon.parent = this.transform;
+        firstCanyon.transform.localPosition = new Vector3(0, 0, 0);
+
+        Vector3 newPos = new Vector3(firstCanyon.transform.Find("Anchor").transform.position.x,
+                                        firstCanyon.transform.Find("Anchor").transform.position.y,
+                                        firstCanyon.transform.Find("Anchor").transform.position.z);
+
+        Transform secondCanyon = (Transform)Instantiate(canyonPrefab, newPos, new Quaternion());
+        secondCanyon.parent = this.transform;
+        secondCanyon.Find("TriggerWall").GetComponent<SpawnerWall>().setLastCanyon(firstCanyon.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+	}
+}
---
+++ Assets/SpawnerWall.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerWall : MonoBehaviour {
+
+    public Transform nextCanyon;
+
+    private GameObject lastCanyon;
+
+    void Update()
+    {
+        if (this.transform.position.z < GameObject.Find("Camera").transform.position.z)
+        {
+            //The last tile can be destroy
+            Destroy(lastCanyon);
+        }
+    }
+
+	void OnTriggerEnter (Collider other) {
+	    if(other.name == "airplane_01")
+        {
+            //The plane has reached this wall: we have to spawn a new tile
+
+            Debug.Log("collision with plane");
+
+            //Spawn a new tile after this one
+            Vector3 newPos = new Vector3(this.transform.parent.Find("Anchor").transform.position.x,
+                                        this.transform.parent.Find("Anchor").transform.position.y,
+                                        this.transform.parent.Find("Anchor").transform.position.z);
+            GameObject newCanyon = (GameObject)Instantiate(nextCanyon, newPos ,new Quaternion());
+            newCanyon.transform.parent = this.transform.parent.parent;
+            newCanyon.transform.Find("TriggerWall").GetComponent<SpawnerWall>().setLastCanyon(this.transform.parent.gameObject);
+            
+        }
+	}
+
+    public void setLastCanyon(GameObject canyon)
+    {
+        this.lastCanyon = canyon;
+    }
+}
