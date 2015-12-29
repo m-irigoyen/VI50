b3d4001af9aafa8fab713468f617e91e8538c3a7
ValentinLeMorvan <valentinlemorvan@gmail.com>
2015-12-29 22:11:43 +0100

    Plan can now move left and right

---
+++ Assets/RollController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollController : MonoBehaviour {
+
+    private GameObject controllerNode;
+    public float speed;
+    
+    void Start()
+    {
+        controllerNode = GameObject.Find("HeadNode");
+    }
+
+	// Update is called once per frame
+	void Update () {
+        float roll = controllerNode.transform.eulerAngles.z;
+        
+        this.gameObject.transform.Translate(new Vector3(-speed * roll * Time.deltaTime,0,0));
+        this.gameObject.transform.eulerAngles = new Vector3(this.gameObject.transform.eulerAngles.x,
+                                                            this.gameObject.transform.eulerAngles.y,
+                                                            roll);
+	}
+}
